8a3db9473fecb560b0e762f477e6ae2b3cfce98b
Ansh Agrawal <103252458+AnshAggr1303@users.noreply.github.com>
2025-08-08 02:54:35 +0530

    Update startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,2 +1,7 @@
+# backend/startup.sh
+
+# Navigate to the backend directory
 cd backend
-uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+
+# Use Gunicorn as a process manager for Uvicorn
+gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
